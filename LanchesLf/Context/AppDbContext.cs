84293e8873bb2b843312d5f0a95aaa488e0e7ae8
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2020-09-03 11:24:51 -0400

    Creating and configuring the project structure

---
+++ LanchesLf/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using LanchesLf.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesLf.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {           
+        }
+
+        public DbSet<Lanche> Lanches { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
---
+++ LanchesLf/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace LanchesLf.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        [StringLength(100)]
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+        [StringLength(200)]
+        public List<Lanche> Lanches { get; set; }
+    }
+}
