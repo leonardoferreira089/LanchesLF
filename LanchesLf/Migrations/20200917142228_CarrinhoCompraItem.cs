c06d96ebd3e2b43dfee1590093c9814b6e8bbb67
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2020-09-17 10:24:59 -0400

    implementing partial views

---
+++ LanchesLf/Migrations/20200917142228_CarrinhoCompraItem.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LanchesLf.Migrations
+{
+    public partial class CarrinhoCompraItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
